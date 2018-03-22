f5baa5f7fef3601da7acb314c0d590ae17c07890
Austin Roberts and Brian Henry <austin@>
2018-03-22 14:30:55 -0700

    Final Edits

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,21 @@ $(document).ready(function(){
   // alert("hi");
 
   $("#fields").change(function() {
-    var month = parseInt($("#fields").val());
+    var month = parseInt($("select#fields").val());
+    if (month === 0) {
+      $('#january').hide();
+      $('#february').hide();
+      $('#march').hide();
+      $('#april').hide();
+      $('#may').hide();
+      $('#june').hide();
+      $('#july').hide();
+      $('#august').hide();
+      $('#september').hide();
+      $('#october').hide();
+      $('#november').hide();
+      $('#december').hide();
+    }
     if (month === 1) {
       $('#january').show();
       $('#february').hide();
